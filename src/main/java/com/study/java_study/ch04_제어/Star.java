eb7e8ba8589744ac7cae3e1e196fd3cd16a75a95
insssu <xmrtn210845@naver.com>
2024-05-23 17:54:02 +0900

    부연설명

--- src/main/java/com/study/java_study/ch04_제어/Star.java
+++ src/main/java/com/study/java_study/ch04_제어/Star.java
@@ -76,10 +76,10 @@ public class Star {
         }
 
         for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < i; j++) {
+            for(int j = 0; j < 5 - (i + 1); j++) {
                 System.out.print(" ");
             }
-            for(int k = 0; k < i + i; k++) {
+            for(int k = 0; k < i + (i + 1); k++) {
                 System.out.print("*");
             }
             System.out.println();
